9192ef230140f12c3b94195dacde11d3560b886c
DrtLckd <adznan.fernandez@gmail.com>
2024-07-17 11:04:28 +0800

    Added 'enter' feature to move to next text box

--- src/main/java/com/mycompany/leninsystem/RFQ_createRequest.java
+++ src/main/java/com/mycompany/leninsystem/RFQ_createRequest.java
@@ -16,6 +16,8 @@ import org.bson.Document;
 import javax.swing.SwingWorker;
 import java.util.List;
 import java.util.ArrayList;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 
 /**
@@ -109,6 +111,7 @@ public class RFQ_createRequest extends javax.swing.JFrame {
         jLabel3.setText("Location:");
 
         jLabel4.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        jLabel4.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
         jLabel4.setText("Requested by:");
 
         jLabel5.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
@@ -121,9 +124,23 @@ public class RFQ_createRequest extends javax.swing.JFrame {
                 proj_locationActionPerformed(evt);
             }
         });
+        proj_location.addKeyListener(new KeyAdapter() {
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    contact_no.requestFocus();
+                }
+            }
+        });
 
         client_name.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         client_name.setToolTipText("");
+        client_name.addKeyListener(new KeyAdapter() {
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    proj_location.requestFocus();
+                }
+            }
+        });
 
         send_req_rfq_btn.setText("SUBMIT");
         send_req_rfq_btn.setBorderColor(new java.awt.Color(0, 0, 255));
@@ -140,6 +157,13 @@ public class RFQ_createRequest extends javax.swing.JFrame {
         jLabel6.setText("Client Email:");
 
         client_email.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        client_email.addKeyListener(new KeyAdapter() {
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    request_by.requestFocus();
+                }
+            }
+        });
 
         cancel_btn.setText("CANCEL");
         cancel_btn.setBorderColor(new java.awt.Color(0, 0, 255));
@@ -153,6 +177,13 @@ public class RFQ_createRequest extends javax.swing.JFrame {
         });
 
         request_by.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        request_by.addKeyListener(new KeyAdapter() {
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    send_to_email.requestFocus();
+                }
+            }
+        });
 
         Client_Information.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
         Client_Information.setText("CLIENT INFORMATION");
@@ -162,6 +193,13 @@ public class RFQ_createRequest extends javax.swing.JFrame {
 
         contact_no.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         contact_no.setToolTipText("Number must contain 11 letters, with the first two being \"09\"");
+        contact_no.addKeyListener(new KeyAdapter() {
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    client_email.requestFocus();
+                }
+            }
+        });
 
         jLabel8.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
         jLabel8.setText("Stock Availability:");
@@ -246,7 +284,7 @@ public class RFQ_createRequest extends javax.swing.JFrame {
                                     .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                                     .addComponent(send_to_email, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                                 .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, body_login1Layout.createSequentialGroup()
-                                    .addComponent(jLabel4)
+                                    .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                                     .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                     .addComponent(request_by, javax.swing.GroupLayout.PREFERRED_SIZE, 364, javax.swing.GroupLayout.PREFERRED_SIZE))))
                         .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, body_login1Layout.createSequentialGroup()
@@ -307,10 +345,10 @@ public class RFQ_createRequest extends javax.swing.JFrame {
                     .addComponent(jLabel4)
                     .addComponent(request_by, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(body_login1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jLabel5, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(send_to_email))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 31, Short.MAX_VALUE)
+                .addGroup(body_login1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(send_to_email, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel5, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 28, Short.MAX_VALUE)
                 .addComponent(Client_Information2)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(body_login1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
